76c585db9cea95feba873a6079a0b440827741c2
mayoi <1299824519@qq.com>
2016-11-17 12:28:21 +0800

    add page1.js

--- page1.js
+++ page1.js
@@ -8,6 +8,12 @@
             return this.each (function () {  //这里的this就是jquery对象，这里return是为了支持链式调用
                 var $this=$(this);
                 init();
+
+                function init(){
+                    var htmlStr = setUpHtml();
+                    $this.html(htmlStr);
+                }
+
                 function getTotalPageCount(){
                     //进行数字运算，
                     var count=opts.recordsCount/opts.perPageCount;
@@ -16,12 +22,15 @@
                 };
 
 
+                //页面分页填充字符串
                 function setUpHtml(){
                     var count=getTotalPageCount(),
                         str='',
                         className='';
                     for(var i=1;i<=count;i++){
+                        //第一页数据不存在，返回空
                         className='';
+                        //第一页默认展示选中样式
                         if(i==1){
                             className=opts.selectClass;
                         }
@@ -31,15 +40,12 @@
                     return str;
                 };
 
-                function init(){
-                    var htmlStr = setUpHtml();
-                    $this.html(htmlStr);
-                }
-
-
                 $this.on('click','.page-list li a',function(){
+                    //指向符
                     var num=$(this).text();
+                    //
                     $(this).addClass(opts.selectClass).parent().siblings().find('a').removeClass(opts.selectClass);
+                    //callback返回值，方便插件调用
                     opts.callBack && opts.callBack(num);
                 });
 
@@ -49,8 +55,8 @@
 
     var defaluts = {
         recordsCount: 190,//总页数
-        perPageCount: 20,//分页展示多少页(建议取奇数)
+        perPageCount: 20,//分页展示多少页
         selectClass: 'select' ,//select选中
         callBack : null
     };
-})(jQuery);;
+})(jQuery);
